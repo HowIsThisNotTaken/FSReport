155289a99516cf44af0cecb21b2a1172dca64eca
Linger7 <lingeric95@gmail.com>
2015-01-27 16:23:49 -0500

    Automatic Table Creation
    Automatically generate tables

--- FSReport/lua/fs_report.lua
+++ FSReport/lua/fs_report.lua
@@ -13,10 +13,17 @@ local fs_DBName = "";	//Database Name
 
 local FS_Database = tmysql.initialize(fs_DBHost, fs_DBUserName, fs_Password, fs_DBName, "3306")
 
+//If you have are using this across multiple servers, I recommend setting a server id per server
 local serverID
 timer.Simple(5, function()
 	serverID = sv_identifier or 1
 end)
+
+
+local function fs_report_iDB()
+	FS_Database:Query( "CREATE TABLE IF NOT EXISTS fs_report(playerid VARCHAR(255), time TIMESTAMP, id INTEGER(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, reporterid VARCHAR(255), comment TEXT, playernick VARCHAR(255), reporternick VARCHAR(255), serverid INT(4), resolved INT(1))", fs_FindPlayerRank, 1, Player)
+end
+hook.Add("InitPostEntity", "fs_report_iDB", fs_report_iDB)
 
 util.AddNetworkString("sendReport")
 util.AddNetworkString("sendReportNoPlayer")
@@ -28,7 +35,7 @@ net.Receive("sendReport", function()
 	
 	local str = SQLStr
 	
-	FS_Database:Query( "REPLACE INTO pogreport(playerid, reporterid, comment, playernick, reporternick, serverid) VALUES(".. str(ply:SteamID()) .. "," .. str(reporter:SteamID()) .. "," .. str(info) .. "," .. str(ply:Nick()) .. "," .. str(reporter:Nick()).. "," .. str(serverID) .. ");", nil, 1)
+	FS_Database:Query( "REPLACE INTO fs_report(playerid, reporterid, comment, playernick, reporternick, serverid) VALUES(".. str(ply:SteamID()) .. "," .. str(reporter:SteamID()) .. "," .. str(info) .. "," .. str(ply:Nick()) .. "," .. str(reporter:Nick()).. "," .. str(serverID) .. ");", nil, 1)
 	for k, v in pairs(player.GetAll()) do
 		if v:IsAdmin() then
 			v:ChatPrint("A new report has been submitted on this Server, Reporter: " .. reporter:Nick())
@@ -42,7 +49,7 @@ net.Receive("sendReportNoPlayer", function()
 	local info = net.ReadString()
 	local str = SQLStr
 
-	FS_Database:Query( "REPLACE INTO pogreport(playerid, reporterid, comment, playernick, reporternick, serverid) VALUES(".. str("No Player") .. "," .. str(reporter:SteamID()) .. "," .. str(info) .. "," .. str("No Player") .. "," .. str(reporter:Nick()).. "," .. str(serverID) .. ");", nil, 1)
+	FS_Database:Query( "REPLACE INTO fs_report(playerid, reporterid, comment, playernick, reporternick, serverid) VALUES(".. str("No Player") .. "," .. str(reporter:SteamID()) .. "," .. str(info) .. "," .. str("No Player") .. "," .. str(reporter:Nick()).. "," .. str(serverID) .. ");", nil, 1)
 	for k, v in pairs(player.GetAll()) do
 		if v:IsAdmin() then
 			v:ChatPrint("A new report has been submitted on this Server, Reporter: " .. reporter:Nick())
@@ -66,7 +73,7 @@ end
 
 function requestReportsChat(Player)
 	if Player:IsAdmin() then
-		FS_Database:Query( "SELECT * FROM pogreport ORDER BY time DESC LIMIT 25;", requestReport, 1, Player)
+		FS_Database:Query( "SELECT * FROM fs_report ORDER BY time DESC LIMIT 25;", requestReport, 1, Player)
 	end
 end
 
@@ -74,12 +81,12 @@ util.AddNetworkString("requestReports")
 net.Receive("requestReports", function()
 	local ply = net.ReadEntity()
 	if ply != nil and ply:IsPlayer() then
-		FS_Database:Query( "SELECT * FROM pogreport ORDER BY time DESC LIMIT 25;", requestReport, 1, ply)
+		FS_Database:Query( "SELECT * FROM fs_report ORDER BY time DESC LIMIT 25;", requestReport, 1, ply)
 	end
 end)
 
 util.AddNetworkString("markResolved")
 net.Receive("markResolved", function()
 	local id = net.ReadString()
-	FS_Database:Query( "UPDATE pogreport SET resolved = 1 WHERE id =" .. id .. ";", nil, 1)
+	FS_Database:Query( "UPDATE fs_report SET resolved = 1 WHERE id =" .. id .. ";", nil, 1)
 end)
